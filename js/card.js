b27e66f50269cf3c6b091e3b4ef869d6e945d5b7
Basilick <soyali298@gmail.com>
2019-03-11 15:49:30 -0600

    Add files via upload

---
+++ js/card.js
@@ -0,0 +1,22 @@
+new Card({
+  form: 'form',
+  container: '.card',
+  formSelectors: {
+    numberInput: 'input[name=number]',
+    expiryInput: 'input[name=expiry]',
+    expiryInput2: 'input[name=expiry2]',	  
+    cvcInput: 'input[name=cvv]',
+    nameInput: 'input[name=name]'
+  },
+
+  width: 390, // optional — default 350px
+  formatting: true,
+
+  placeholders: {
+    number: '•••• •••• •••• ••••',
+    name: 'Nombre Tarjeta',
+    expiry: '••',
+	expiry2: '••••',  
+    cvc: '•••'
+  }
+})
